8d108534f5d487c04a38ef3efc5eb2546108720c
TsAlexandr <lunoface@mail.ru>
2022-04-15 19:33:58 +0700

    i forget to add this file

---
+++ src/repositories/videos-db-repository.ts
@@ -0,0 +1,34 @@
+import {videos} from "./db";
+import {VideoType} from "./db";
+
+export const videosRepository = {
+    getVideos() {
+        return videos
+    },
+    getVideoById(id: number) {
+        const video = videos.find(v => v.id === id)
+        return video
+    },
+    deleteVideoById(id: number) {
+        for (let i = 0; i < videos.length; i++) {
+            if (videos[i].id === id) {
+                videos.splice(i, 1)
+                return true
+            }
+        }
+        return false
+    },
+    updateVideoById(id: number, title: string) {
+        const video = videos.find(v => v.id === id)
+        if (video) {
+            video.title = title
+            return true
+        } else {
+            return false
+        }
+    },
+    createVideo(newVideo: VideoType) {
+        videos.push(newVideo)
+        return newVideo
+    }
+}
